5e2b01f972f8ee31474982ad1003e4c140f12bfe
Lourdes Villalba <vlourdes96@gmail.com>
2025-07-03 16:05:50 -0300

    Arreglo de ruta inicio

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,6 @@ use App\Livewire\PostCrud;
 use App\Models\Post; // Asegúrate de que este import esté presente
 
 // Ruta para la página de inicio
-// Esta ruta siempre cargará la vista 'inicio' y le pasará los posts.
-// La lógica de autenticación (mostrar botón de crear post o enlaces de login/registro)
-// se maneja dentro de la vista 'inicio.blade.php' usando @auth.
 Route::get('/', function () {
     $posts = Post::latest()->get(); // Obtiene los posts más recientes
     return view('welcome', compact('posts')); // Asegúrate de que el nombre de la vista sea 'inicio'
